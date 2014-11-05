a95b08a8d5f07983c25a8a0ca355cd013c6ba8b8
chenshuo <chenshuox@126.com>
2014-11-05 21:24:31 +0800

    完善

--- application.php
+++ application.php
@@ -4,31 +4,34 @@ namespace simple;
 class Application
 {
 	private $route;
+	private $control;
+	private $action;
 
 	public function __construct() {
 		require "request/pathinfo.php";
 		$this->route = new \simple\request\Pathinfo();
+		$this->init();
 	}
-
+	private function init() {
+		$this->control();
+		$this->action();
+	}
 	private function control() {
 		if(empty($this->route->get(1))) {
-			return "home";
+			return $this->control = "home";
 		}
-		return $this->route->get(1);
+		return $this->control = $this->route->get(1);
 	}
 
 	private function action() {
 		if(empty($this->route->get(2))) {
-			return "index";
+			return $this->action = "index";
 		}
-		return $this->route->get(2);
+		return $this->action = $this->route->get(2);
 	}
 
 	public function run() {
-		$control = $this->control();
-		$controller = new $control();
-		$action = $this->action();
-		$controller -> $action();
+		
 	}
 
 }
