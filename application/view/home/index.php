704052edb4de857e354fb4525463141a3572c11c
chenshuo <chenshuox@126.com>
2014-12-06 11:44:40 +0800

    修改
    修改

--- application/view/home/index.php
+++ application/view/home/index.php
@@ -2,7 +2,26 @@
 <html>
 <head>
 	<title>陈朔的个人框架</title>
-	<meta charset="utf-8">
+	<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=7" />
+	<style type="text/css">
+	.header{
+		width: 960px;
+		height: 90px;
+		background: #000;
+		margin: 0 auto;
+		
+	}
+	.content{
+		margin-left: -10px;
+		background: #f00;
+	}
+	.show{
+		margin-left: 10px;
+		height: 10px;
+		background: #09c;
+	}
+	</style>
 </head>
 <body>
 	<h2>simple</h2>
@@ -16,5 +35,10 @@
 			<li><input type="submit" value="login"></li>
 		</ul>
 	</form>
+	<div class="header">
+		<ul class="content">
+			<li class="show"></li>
+		</ul>
+	</div>
 </body>
 </html>
