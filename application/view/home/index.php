22f55defe7f051af2dd9968f59ecf6f9482ccc57
chenshuo <chenshuox@126.com>
2015-01-26 19:18:40 +0800

    为了同步而修改

--- application/view/home/index.php
+++ application/view/home/index.php
@@ -6,13 +6,10 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=7" />
 </head>
 <body>
-<<<<<<< HEAD
-	<h2>足球迷 zuqiu.im</h2>
-	<p>喜欢足球</p>
-=======
+
 	<h2><?php echo $webName; ?></h2>
 	<p><?php echo $description; ?></p>
->>>>>>> FETCH_HEAD
+
 	<hr>
 	<form action="<?=HTTP_PATH?>/home/index" method="post">
 		<ul>
@@ -26,9 +23,6 @@
 	<ul>
 		<li><a href="<?=HTTP_PATH?>/home/show">chenshuo</a></li>
 	</ul>
-<<<<<<< HEAD
-	
-=======
 	<script type="text/javascript">
 	var color = document.getElementById('color');
 	var text = document.getElementById('text');
@@ -38,6 +32,6 @@
 	}
 	</script>
 	<p><?php echo $powered; ?></p>
->>>>>>> FETCH_HEAD
+
 </body>
 </html>
