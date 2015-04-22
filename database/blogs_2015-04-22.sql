5d43119aeb64705d0d9db9a4fa3b0bef84c86b3d
chenshuo <chenshuox@126.com>
2015-04-22 08:26:53 +0800

    新增数据库同步

---
+++ database/blogs_2015-04-22.sql
@@ -0,0 +1,88 @@
+﻿# ************************************************************
+# Sequel Pro SQL dump
+# Version 4096
+#
+# http://www.sequelpro.com/
+# http://code.google.com/p/sequel-pro/
+#
+# Host: localhost (MySQL 5.5.38)
+# Database: blogs
+# Generation Time: 2015-04-22 00:25:16 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table category
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `category`;
+
+CREATE TABLE `category` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `parentid` int(11) DEFAULT NULL,
+  `name` varchar(20) NOT NULL DEFAULT '',
+  `english` varchar(20) DEFAULT NULL,
+  `description` varchar(40) DEFAULT NULL,
+  `path` varchar(10) NOT NULL DEFAULT '',
+  `type` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+
+INSERT INTO `category` (`id`, `parentid`, `name`, `english`, `description`, `path`, `type`)
+VALUES
+	(1,0,'生活','Life','记录生活点滴','0-',0),
+	(2,0,'码农','code','程序猿的一天','0-',0),
+	(3,2,'PHP','php','php最优雅的web开发语言','0-2-',0),
+	(4,2,'html','html','超文本标记语言','0-2-',0),
+	(5,2,'css',NULL,NULL,'0-2-',0),
+	(6,2,'javascript','javascript',NULL,'0-2-',0),
+	(7,2,'MySQL','mysql','我的数据库知识','0-2-',0),
+	(8,2,'c/c++','c','c/c++知识','0-2-',0),
+	(9,0,'Photo','photo','照片记录生活','0-',1),
+	(10,0,'关于我','about','关于陈朔','0-',0);
+
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table manage
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `manage`;
+
+CREATE TABLE `manage` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL DEFAULT '',
+  `password` varchar(40) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `manage` WRITE;
+/*!40000 ALTER TABLE `manage` DISABLE KEYS */;
+
+INSERT INTO `manage` (`id`, `username`, `password`)
+VALUES
+	(1,'admin','21232f297a57a5a743894a0e4a801fc3');
+
+/*!40000 ALTER TABLE `manage` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
