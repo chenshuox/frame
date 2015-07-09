414d51ac0e5ff6a71a3ef34c0fc13313b630bd2d
chenshuo <chenshuox@126.com>
2015-07-09 22:40:01 +0800

    修改路由
    修改

--- system/core/controller/route.php
+++ system/core/controller/route.php
@@ -1,10 +1,9 @@
 <?php
 namespace simple\system\core\controller;
-use simple\system\core\request;
 use simple\system\core\common;
+use simple\system\core\request;
 
-class Route
-{	
+class Route {
 	private $route;
 	public $control;
 	public $action;
@@ -21,7 +20,7 @@ class Route
 	//获取控制器
 	private function control() {
 		$control = $this->route->get(1);
-		if(empty($control)) {
+		if (empty($control)) {
 			return $this->control = $this->data["default"]["controller"];
 		}
 		return $this->control = $this->route->get(1);
@@ -30,7 +29,7 @@ class Route
 	//获取方法
 	private function action() {
 		$action = $this->route->get(2);
-		if(empty($action)) {
+		if (empty($action)) {
 			return $this->action = $this->data["default"]["action"];
 		}
 		return $this->action = $this->route->get(2);
